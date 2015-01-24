4c957f006db5ba95750bcdf4fe97dd983a46f4a1
AKIFUMI NAMBU <nmbakfm@inter-arteq.com>
2015-01-24 18:00:07 +0900

    MusicScoreクラスを作成

---
+++ MusicScore.h
@@ -0,0 +1,20 @@
+//
+//  MusicScore.h
+//  gomirhythm
+//
+//  Created by 南部 晃史 on 2015/01/24.
+//
+//
+
+#ifndef __gomirhythm__MusicScore__
+#define __gomirhythm__MusicScore__
+
+#include "ofMain.h"
+
+class MusicScore {
+    
+public:
+    MusicScore();
+};
+
+#endif /* defined(__gomirhythm__MusicScore__) */
