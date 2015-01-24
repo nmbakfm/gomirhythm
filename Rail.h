62dbc1975d444f75853399fd3494fb9423b64e5e
AKIFUMI NAMBU <nmbakfm@inter-arteq.com>
2015-01-24 17:58:25 +0900

    Railクラスを作成

---
+++ Rail.h
@@ -0,0 +1,21 @@
+//
+//  Rail.h
+//  gomirhythm
+//
+//  Created by 南部 晃史 on 2015/01/24.
+//
+//
+
+#ifndef __gomirhythm__Rail__
+#define __gomirhythm__Rail__
+
+#include "ofMain.h"
+
+class Rail {
+    
+public:
+    Rail();
+    void draw();
+};
+
+#endif /* defined(__gomirhythm__Rail__) */
